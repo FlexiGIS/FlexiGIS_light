9c9fe01e99de9cc08c52916c2c2ccde0be89189b
chunai <uniachi.chinonso@gmail.com>
2020-02-19 15:36:16 +0100

    add esy-osmfilter module

---
+++ esyfilter-light/README.md
@@ -0,0 +1,16 @@
+# FlexiGIS-Light Using esy-osmfilter
+
+This module implements esy-osmfilter.
+
+Before running the make commands in this module, first install esy-osmfilter package in your python environment by running.
+
+```
+pip install esy-osmfilter
+
+```
+afterwards cd into the code directory in your linux terminal and run
+
+```
+make all
+```
+The output files are located in the ../data/output_data directory. The highway data are saved as csv and .shp file.
---
+++ esyfilter-light/code/config.mk
@@ -0,0 +1,16 @@
+
+# 1. URL of the OSM raw data (used for OSM raw data download)
+OSM_raw_data_URL:=https://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf
+# https://download.geofabrik.de/europe/germany/berlin.poly
+# 2. Name of the OSM raw data file (used for data filtering by osmosis)
+OSM_raw_data:=../data/input_raw_data/berlin-latest.osm.pbf
+
+# 3. Name of the bounding polygon file (used for data filtering by osmosis)
+#    Use other polyfiles for other spatial areas
+polyfile:=../data/input_raw_data/berlin.poly
+
+# 4. Name of the filtered OSM urban data file (used for data export by osm2pgsql)
+OSM_merged_data:=../data/input_raw_data/02-UrbanInfrastructure.osm.pbf
+
+# 6. Location of the output folders
+urban_output:=../data/output_data/berlin_line.geojson
